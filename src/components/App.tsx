e360b2216bcdd87702918d6fe89e8a8f9020b7e6
jhermen-List <60341011+jhermen-List@users.noreply.github.com>
2025-06-30 10:23:04 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1664,10 +1664,10 @@ class App extends React.Component<AppProps, AppState> {
 
   
   private isMobileBreakpoint = (width: number, height: number) => {
-    return (
     if (new URLSearchParams(window.location.search).has("nomobile")) {
       return false;
     } 
+    return (
       width < MQ_MAX_WIDTH_PORTRAIT ||
       (height < MQ_MAX_HEIGHT_LANDSCAPE && width < MQ_MAX_WIDTH_LANDSCAPE)
     );
